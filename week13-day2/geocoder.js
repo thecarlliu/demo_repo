5d6225b9ef295de8069579610b8f9eb5c4165b56
CarL Liu <thecarlliu99@er-10-132-23-243.wireless.umn.edu>
2018-01-31 21:16:34 -0600

    indentation

--- week13-day2/geocoder.js
+++ week13-day2/geocoder.js
@@ -2,34 +2,33 @@ var inquirer = require("inquirer");
 
 var geocoder = require("geocoder");
 
-inquirer
-    .prompt([
-        // Here we create a basic text prompt.
-        {
-            type: "input",
-            message: "Where do you want to go?",
-            name: "location"
-        },
-        {
-            type: "confirm",
-            message: "Are you sure:",
-            name: "confirm",
-            default: true
-        }
-        ])
-    .then (function(inquirerResponse) {
-        if (inquirerResponse.confirm) {
-            var input = inquirerResponse.location;
-            var string = JSON.stringify(input);
+inquirer.prompt([
+    // Here we create a basic text prompt.
+    {
+        type: "input",
+        message: "Where do you want to go?",
+        name: "location"
+    },
+    {
+        type: "confirm",
+        message: "Are you sure:",
+        name: "confirm",
+        default: true
+    }
+    ])
+.then (function(inquirerResponse) {
+    if (inquirerResponse.confirm) {
+        var input = inquirerResponse.location;
+        var string = JSON.stringify(input);
 
-            geocoder.geocode(string, function (err, data) {
-                console.log("Here's info on your destination:\n"+JSON.stringify(data));
-            });
-        }
-        else {
-            console.log("Decide already!");
-        }
-    });
+        geocoder.geocode(string, function (err, data) {
+            console.log("Here's info on your destination:\n"+JSON.stringify(data));
+        });
+    }
+    else {
+        console.log("Decide already!");
+    }
+});
 
 //version without inquirer
 //geocoder.geocode(process.argv.slice(2).join(" "), function (err, data) {
