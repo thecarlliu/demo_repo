5b212c0c6509afab0da625742f21fb645f636003
CarL Liu <thecarlliu99@er-10-132-83-232.wireless.umn.edu>
2018-02-07 20:38:41 -0600

    finished weatheradmin

--- week14-day2/complex.js
+++ week14-day2/complex.js
@@ -3,13 +3,14 @@ var weather = require("weather-js");
 
 var access = process.argv[2];
 var name = process.argv[3];
-var location = process.argv.splice(4);
+var location = String(process.argv.splice(4));
+location = location.replace(",", " ");
 
 function userSearch(name, location) {
     var date = new Date();
     this.name = name;
     this.location = String(location);
-    this.date = date.getMonth()+"-"+date.getDate();
+    this.date = date.getMonth()+1+"-"+date.getDate();
     this.getWeather = function() {
         weather.find({search: location, degreeType: "F"}, function(err, result) {
             if (err) {
@@ -25,12 +26,9 @@ function userSearch(name, location) {
 if (access === "user") {
     var input = new userSearch(name, location);
     input.getWeather();
-    fs.appendFile("log.txt", JSON.stringify(input)+",", function(err) {
+    fs.appendFile("log.txt", "Name: "+input.name+" Location: "+input.location+" Date: "+input.date+"\n", function(err) {
         if (err) {
             return console.log(err);
-        }
-        else {
-            console.log(input);
         }
     });
 }
@@ -40,7 +38,10 @@ else if (access === "admin") {
             return console.log(error);
         }
         else {
-            console.log("["+data+"]");
+            var arr = data.split("\n");
+            for (i=0;i<arr.length-1;i++) {
+                console.log(arr[i]);
+            }
         }
     });
 }
