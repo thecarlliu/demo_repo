ab70a745ac9b9088684352d0edfa491dd0d4d95e
CarL Liu <thecarlliu99@er-10-132-55-230.wireless.umn.edu>
2018-02-17 13:15:52 -0600

    serverfavorites

---
+++ week15-day3/server-fav.js
@@ -0,0 +1,47 @@
+var http = require("http");
+var fs = require("fs");
+
+var PORT = 8080;
+
+var server = http.createServer(handleRequest);
+
+// Start our server
+server.listen(PORT, function() {
+    // Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:" + PORT);
+});
+
+// Create a function which handles incoming requests and sends responses
+function handleRequest(req, res) {
+
+    // Capture the url the request is made to
+    var path = req.url;
+
+    // Depending on the URL, display a different HTML file.
+    switch (path) {
+
+        case "/food":
+            return fs.readFile(__dirname + "/food.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+
+        case "/movies":
+            return fs.readFile(__dirname + "/movies.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+
+        case "/css":
+            return fs.readFile(__dirname + "/css.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+
+        default:
+            return fs.readFile(__dirname + "/index.html", function(err, data) {
+                res.writeHead(200, { "Content-Type": "text/html" });
+                res.end(data);
+            });
+    }
+}
