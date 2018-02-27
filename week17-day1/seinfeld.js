2005f761912d56abc662397006975e3171cca41d
CarL Liu <thecarlliu99@er-10-132-189-72.wireless.umn.edu>
2018-02-26 20:16:54 -0600

    finished

--- week17-day1/seinfeld.js
+++ week17-day1/seinfeld.js
@@ -13,20 +13,10 @@ connection.connect();
 
 var app = express();
 
-function coolSort(array){
-    var temp;
-    for (i=0;i<array.length;i++) {
-        if (array[i].coolness_points < array[i+1]) {
-
-            array[i] = array[i+1];
-        }
-    }
-}
-
 app.get("/cast", function(req, res) {
 //displays all actors and data ordered by their id's
     connection.query("SELECT * FROM actors order by id", function(err, res) {
-        var html = "<h1>Actors Ordered Y ID</h1>";
+        var html = "<h1>Actors Ordered by ID</h1>";
         html+="<ul>";
         for (i=0;i<res.length;i++){
             html+="<li><p> ID: " + res[i].id +"</p>";
@@ -41,9 +31,8 @@ app.get("/cast", function(req, res) {
 
 app.get("/coolness-chart", function(req, res) {
 //displays all actors and data ordered by their coolness points
-    displayArray = [];
     connection.query("SELECT * FROM actors order by coolness_points DESC", function(err, res) {
-        var html = "<h1>Actors Ordered Y ID</h1>";
+        var html = "<h1>Actors Ordered by Coolness</h1>";
         html+="<ul>";
         for (i=0;i<res.length;i++){
             html+="<li><p> ID: " + res[i].id +"</p>";
@@ -58,18 +47,20 @@ app.get("/coolness-chart", function(req, res) {
 
 app.get("/attitude-chart/:att", function(req, res) {
     var attitude = req.params.att;
-
-    switch(attitude) {
-        case "attitude1":
-            break;
-        case "attitude2":
-            break;
-        case "attitude3":
-            break;
-        case "attitude4":
-            break;
-    }
+    connection.query("SELECT * FROM actors WHERE attitude=?", [attitude], function(err, res) {
+        var html = "<h1>Actors with an Attitude of "+attitude+"</h1>";
+        html+="<ul>";
+        for (i=0;i<res.length;i++){
+            html+="<li><p> ID: " + res[i].id +"</p>";
+            html+="<p> Name: "+res[i].actor_name+"</p>";
+            html+="<p> Coolness Points: "+res[i].coolness_points+"</p>";
+            html+="<p> Attitude: "+res[i].attitude+"</p></li>";
+        }
+        html+="</ul>";
+        res.send(html);
+    });
 });
+
 // Initiate the listener.
 app.listen(3002);
 console.log("server is working..");
