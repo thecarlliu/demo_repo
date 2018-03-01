b1aac3113338ddb1e309285731dc8cf3e8ae7d44
CarL Liu <thecarlliu99@er-10-132-66-69.wireless.umn.edu>
2018-02-28 19:06:56 -0600

    finished warmup

--- week17-day2/warmup/server.js
+++ week17-day2/warmup/server.js
@@ -31,25 +31,39 @@ var animals = [
   }
 ];
 
+var anims = [];
+
 app.get("/dog", function(req, res) {
   // Handlebars requires an object to be sent to the dog.handlebars file. Lucky for us, animals[0] is an object!
 
   // 1. Send the dog object from the animals array to the dog.handlebars file.
+    res.render("dog", animals[0]);
 
 });
 
 app.get("/all-pets", function(req, res) {
   // Handlebars requires an object to be sent to the index.handlebars file.
-
-  // 2. Send the animals to the index.handlebars file. Remember that animals is an array and not an object.
-
+    anims = [];
+  // 2. Send the animals that are pets to the index.handlebars file. Remember that animals is an array and not an object.
+    for (i=0;i<animals.length;i++) {
+        if (animals[i].pet===true){
+            anims.push(animals[i]);
+        }
+    }
+    res.render("index", {pets: anims});
 });
 
 app.get("/all-non-pets", function(req, res) {
   // Handlebars requires an object to be sent to the index.handlebars file.
 
   // 3. Send all the animals that are not pets to the index.handlebars file.
-
+    anims = [];
+    for (i=0;i<animals.length;i++) {
+        if (animals[i].pet===false){
+            anims.push(animals[i]);
+        }
+    }
+    res.render("index", {pets: anims});
 });
 
 app.listen(port);
